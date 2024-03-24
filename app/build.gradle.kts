915ebd10bf91eac812d6144ac3dd695df9da6315
MuhammadKurniawanDwiHariyadi <210441100167@student.trunojoyo.ac.id>
2024-03-24 13:04:56 +0700

    Penambahan Dexter

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation ("com.karumi:dexter:6.2.3")
 }
